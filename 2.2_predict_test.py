35b14080730c217a6f0330ec8a2673e83416d876
kavidey <kaviasher@gmail.com>
2025-09-20 18:43:35 +0000

    Update plot generation and data saving

--- 2.2_predict_test.py
+++ 2.2_predict_test.py
@@ -38,16 +38,31 @@ std_inc = pred_dist.scale
 
 test_indices = testX_e.index.tolist()
 
-df_ngb = pd.DataFrame(list(zip(testY_e, pred_e, std_e, testY_inc, pred_inc, std_inc)), columns = ["actual_dele", "pred_e", "error_e", "actual_delsini", "pred_inc", "error_inc"])
+df_ngb = pd.DataFrame(list(zip(testY_e, pred_e, std_e, testY_inc, pred_inc, std_inc)), columns = ["actual_dele", "pred_dele", "error_e", "actual_delsini", "pred_delsini", "error_sini"])
 df_ngb = df_ngb.reset_index(drop=True)
 test_data = merged_df.loc[test_indices].reset_index(drop=True)
 
 df_ngb["Des'n"] = test_data["Des'n"]
+
+# oscillating
 df_ngb["e"] = test_data["e"]
 df_ngb["Incl."] = test_data["Incl."]
+
+# linear
+df_ngb["prope_h"] = test_data["prope_h"]
+df_ngb["propsini_h"] = test_data["propsini_h"]
+
+# predicted
+pred_e = df_ngb["pred_dele"] + df_ngb["e"]
+pred_e[pred_e < 0] = 0 # clamp values less than 0
+df_ngb["pred_e"] = pred_e
+
+pred_sini = df_ngb["pred_delsini"] + np.sin((df_ngb["Incl."] * np.pi/180))
+df_ngb["pred_sini"] = pred_sini
+
+# acutal
 df_ngb["propa"] = test_data["propa"]
 df_ngb["prope"] = test_data["prope"]
-df_ngb["prope_h"] = test_data["prope_h"]
 df_ngb["propsini"] = test_data["propsini"]
-df_ngb["propsini_h"] = test_data["propsini_h"]
+
 df_ngb.to_csv("data/model_results.csv")
