318c5aef4e08d3db5b2ee07c878236e557a4e9f0
lshen <lshen@hmc.edu>
2025-10-24 18:03:08 +0000

    change the 5_2 file with assist implementation

--- 5_uncertainty_testing/5.2_integrate_asteroids.py
+++ 5_uncertainty_testing/5.2_integrate_asteroids.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import rebound as rb
+import celmech as cm
+import assist
 
-import sys
-sys.path.insert(0, 'SBDynT/src')
-import sbdynt as sbd
+# %%
+ephem = assist.Ephem("../data/assist/linux_m13000p17000.441", "../data/assist/sb441-n16.bsp")
 
+# %%
 outer_only = True
 print(f"Outer Planets Only: {outer_only}")
 # %%
@@ -19,72 +21,86 @@ integration_path.mkdir(parents=True, exist_ok=True)
 # %%
 merged_df = pd.read_csv("../data/uncertainty_asteroids_sampled.csv", index_col=0)
 start_time = 2460200.5
+
 # %%
-sim = rb.Simulation()
-
-(flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='sun',epoch=start_time)
-if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='sun')
 if not outer_only:
-    (flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='mercury',epoch=start_time)
-    if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='mercury')
+    planets_list = [
+    "Sun",
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune"
+	]
 
-    (flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='venus',epoch=start_time)
-    if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='venus')
+planets_list = [
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune"
+]
+    
 
-    (flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='earth',epoch=start_time)
-    if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='earth')
+# %%
+def add_planets(sim):
+	for name in planets_list:
+		planet = ephem.get_particle(name, start_time - ephem.jd_ref)
+		sim.add(planet)
 
-    (flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='mars',epoch=start_time)
-    if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='mars')
-
-(flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='jupiter',epoch=start_time)
-if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='jupiter')
-
-(flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='saturn',epoch=start_time)
-if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='saturn')
-
-(flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='uranus',epoch=start_time)
-if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='uranus')
-
-(flag, mass, radius, [plx,ply,plz],[plvx,plvy,plvz]) = sbd.query_horizons_planets(obj='neptune',epoch=start_time)
-if flag: sim.add(m=mass, x=plx, y=ply, z=plz, vx=plvx/(np.pi*2), vy=plvy/(np.pi*2), vz=plvz/(np.pi*2), hash='neptune')
-
-if outer_only:
-    assert len(sim.particles) == 5, "Error adding planets"
-else:
-    assert len(sim.particles) == 9, "Error adding planets"
-
-sim.save_to_file(str(integration_path/"planets.bin"))
 # %%
 def run_sim(r):
-    idx, row = r
+	idx, row = r
 
-    sim = rb.Simulation(str(integration_path/'planets.bin'))
-    rel_int_time = (row["epoch"]-start_time) / ((1/(2.*np.pi))*365.25)
-    sim.integrate(rel_int_time)
-    sim.move_to_hel()
+	# Integrate backwards to Nesvorny epoch
+	sim = rb.Simulation()
+	ex = assist.Extras(sim, ephem)
+	sim.t = row['epoch'] - ephem.jd_ref
+	sim.add(x=row['x'], y=row['y'], z=row['z'], 
+		 vx=row['vx']/(np.pi*2), vy=row['vy']/(np.pi*2), vz=row['vz']/(np.pi*2))
+	sim.move_to_com()
+	sim.integrate(start_time - ephem.jd_ref)
+	sim.exit_max_distance = 50.
 
-    sim.add(x=row['x'], y=row['y'], z=row['z'], vx=row['vx']/(np.pi*2), vy=row['vy']/(np.pi*2), vz=row['vz']/(np.pi*2))
-    sim.move_to_com()
+	# Create a new simulation and integrate forward
+	ps = sim.particles
+	ps_asteroid = ps[-1]
+	ps_final = {
+		'x': ps_asteroid.x,
+		'y': ps_asteroid.y,
+		'z': ps_asteroid.z,
+		'vx': ps_asteroid.vx,
+		'vy': ps_asteroid.vy,
+		'vz': ps_asteroid.vz
+	}
 
-    sim.exit_max_distance = 50.
+	sim2 = rb.Simulation()
+	ex2 = assist.Extras(sim2, ephem)
+	add_planets(sim2)
+	sim2.move_to_hel()
+	sim2.t = start_time - ephem.jd_ref
 
-    ps = sim.particles
-    ps[-1].a
+	sim2.add(x=ps_final['x'], y=ps_final['y'], z=ps_final['z'], 
+		  vx=ps_final['vx'], vy=ps_final['vy'], vz=ps_final['vz'])
+	
+	ps2 = sim2.particles
+	sim2.integrator = 'whfast'
+	sim2.dt = ps2[1].P/100.
+	sim2.ri_whfast.safe_mode = 0
 
-    sim.integrator='whfast'
-    sim.dt = ps[1].P/100.
-    sim.ri_whfast.safe_mode = 0
+	Tfin_approx = 3e7*ps2[-1].P
+	total_steps = np.ceil(Tfin_approx / sim2.dt)
+	Tfin = total_steps * sim2.dt + sim2.dt
+	Nout = 25_000
 
-    Tfin_approx = 3e7*ps[-1].P
-    total_steps = np.ceil(Tfin_approx / sim.dt)
-    Tfin = total_steps * sim.dt + sim.dt
-    Nout = 25_000
+	sim_file = integration_path / f"asteroid_integration_{row["Des'n"]}-{idx}.sa"
+	sim2.save_to_file(str(sim_file), step=int(np.floor(total_steps/Nout)), delete_file=True)
+	sim2.integrate(Tfin, exact_finish_time=0)
 
-    sim_file = integration_path / f"asteroid_integration_{row["Des'n"]}-{idx}.sa"
-    sim.save_to_file(str(sim_file), step=int(np.floor(total_steps/Nout)), delete_file=True)
-    sim.integrate(Tfin, exact_finish_time=0)
 # %%
 with Pool(40) as p:
       p.map(run_sim, merged_df.iterrows())
-# %%
+
+
