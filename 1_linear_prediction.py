Fix bug in rotation of invariable frame for linear theory and clean up train/test split code

1_linear_prediction.py
@@ -81,14 +81,14 @@ def ecc_inc_prediction(r):
 	# Calculate linear theory
 	u0, v0, g0, s0 = linear_theory_prediction(orb.e, orb.inc, orb.pomega, orb.Omega, orb.a, simpler_secular_theory)
 
+	# rotate the proper elements back into the original frame
 	sim.particles[-1].e = np.clip(np.abs(u0), 0, 0.99)
 	sim.particles[-1].inc = np.abs(v0)
 	sim.rotate(rot.inverse())
 	p = sim.particles[-1]
-	orb = p.orbit(primary=sim.particles[0])
+	orb2 = p.orbit(primary=sim.particles[0])
 
-	# From now on, we will use the invariable frame elements as the oscullating elements
-	return row["Des'n"], u0, v0, g0, s0, orb.a, orb.e, np.degrees(orb.inc), np.degrees(orb.Omega), np.degrees(orb.pomega)
+	return row["Des'n"], u0, v0, g0, s0, orb.e, orb.inc
 # %%
 start_t = time.process_time()
 ncpus = 40
@@ -98,5 +98,8 @@ eval_t = (time.process_time() - start_t) * ncpus
 print(f"Linear Theory Time: {eval_t:.2f} sec for {len(nesvorny_df)} asteroids. {eval_t/len(nesvorny_df):.4} sec / asteroid")
 # Linear Theory Time: 12229.41 sec for 1249051 asteroids. 0.009791 sec / asteroid
 # %%
-df_all = pd.DataFrame(table, columns=["Des'n", "u0", "v0", "g0", "s0", "a", "e", "Incl.", "Node", "Peri."])
+# u0,v0 are the complex proper elements in the invariable frame
+# g0,s0 are the proper frequencies
+# prope_linear, propi_linear are the proper elements in the original frame
+df_all = pd.DataFrame(table, columns=["Des'n", "u0", "v0", "g0", "s0", "prope_linear", "propi_linear"])
 df_all.to_csv("data/linear_theory.csv")

2.1_train_xgboost.py
@@ -13,21 +13,27 @@ from xgboost.sklearn import XGBRegressor
 nesvorny_df = pd.read_csv("data/nesvorny_catalog_dataset.csv", index_col=0, dtype={"Des'n": str})
 
 # Read linear prediction results
+# u0,v0 are the complex proper elements in the invariable frame
+# g0,s0 are the proper frequencies
+# prope_linear, propi_linear are the proper elements in the original frame
 linear_theory_df = pd.read_csv("data/linear_theory.csv", index_col=0, dtype={"Des'n": str})
+# parse complex columns
 linear_theory_df['u0'] = linear_theory_df['u0'].apply(lambda x: complex(x))
 linear_theory_df['v0'] = linear_theory_df['v0'].apply(lambda x: complex(x))
-linear_theory_df['prope_h'] = np.abs(linear_theory_df['u0'])
-linear_theory_df['propsini_h'] = np.sin(np.abs(linear_theory_df['v0']))
+# calculate linear elements
+linear_theory_df['propsini_linear'] = np.sin(linear_theory_df["propi_linear"])
+# rename linear frequencies
+linear_theory_df['g_linear'] = linear_theory_df['g0']
+linear_theory_df['s_linear'] = linear_theory_df['s0']
 # %%
 # Get merged dataframe for later model training
-merged_df = pd.merge(nesvorny_df, linear_theory_df, on="Des'n", how="inner")
+merged_df = pd.merge(nesvorny_df[["Des'n", "propa", "da", "prope", "de", "propsini", "dsini", "g", "s"]],
+                     # get proper elements from the nesvorny'24 data set
+                     linear_theory_df[["Des'n", "prope_linear", "propsini_linear", "g_linear", "s_linear",
+                                       "a", "e", "Incl.", "Node", "Peri."]],
+                     # and linear & oscullating (in the invariable frame) elements from the linear theory file
+                     on="Des'n", how="inner")
 
-merged_df["prope_h"] = np.abs(merged_df["prope_h"])
-merged_df["propsini_h"] = np.abs(merged_df["propsini_h"])
-merged_df['prope_h'] = pd.to_numeric(merged_df['prope_h'], errors='coerce')
-merged_df['propsini_h'] = pd.to_numeric(merged_df['propsini_h'], errors='coerce')
-merged_df['Node'] = pd.to_numeric(merged_df['Node'], errors='coerce')
-merged_df['Peri.'] = pd.to_numeric(merged_df['Peri.'], errors='coerce')
 
 node = np.deg2rad(merged_df["Node"])
 peri = np.deg2rad(merged_df["Peri."])
@@ -43,8 +49,9 @@ merged_df.to_csv("data/merged_elements.csv")
 # Read merged dataframe for model training
 merged_df = pd.read_csv("data/merged_elements.csv", index_col=0, dtype={"Des'n": str})
 # %%
-features_e = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'prope_h']
-features_inc = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'propsini_h']
+# calculate train / test split for the features used to train each model
+features_e = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'prope_linear']
+features_inc = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'propsini_linear']
 data_e = merged_df[features_e]
 data_inc = merged_df[features_inc]
 dele = merged_df['prope']-merged_df['e']
@@ -52,6 +59,11 @@ delsini = merged_df['propsini']-np.sin(merged_df['Incl.']*np.pi/180)
 
 trainX_e, testX_e, trainX_inc, testX_inc, trainY_e, testY_e, trainY_inc, testY_inc = train_test_split(data_e, data_inc, dele, delsini, train_size=0.8, random_state=42)
 
+# save train/test split back into merged_elements.csv for reproducibility
+merged_df['test_set'] = 0
+merged_df.loc[testX_e.index, "test_set"] = 1
+merged_df.to_csv("data/merged_elements.csv")
+# %%
 param1_grid = {
     'max_depth': np.arange(3, 21.01, 3, dtype=int),
     'min_child_weight': np.arange(1, 4.01, 1, dtype=int),
@@ -64,6 +76,7 @@ param2_grid = {
 }
 # %%
 # COMMENT OUT THIS SECTION TO SKIP HYPERPARAMETER TUNING
+print("Fitting ECC")
 start = time.time()
 grid_search1_e = GridSearchCV(estimator=XGBRegressor(random_state=42, learning_rate=0.3, n_estimators=500, n_jobs=40),
                            param_grid=param1_grid, cv=5, scoring="neg_mean_squared_error", verbose=1)
@@ -76,16 +89,14 @@ grid_search2_e = GridSearchCV(estimator=XGBRegressor(random_state=42, **grid_sea
 grid_search2_e.fit(trainX_e, trainY_e)
 
 end = time.time()
-print(f"Best score: {grid_search2_e.best_score_:.3}")
+print(f"Best score: {grid_search2_e.best_score_:.3}") # this is MSE not RMSE
 print(f"Best parameters: {grid_search1_e.best_params_ | grid_search2_e.best_params_}")
 print("Optimization Time: %.2f seconds" % (end - start))
 # %%
-# final_model_e = grid_search2_e.best_estimator_
-
 # COMMENT OUT TO SKIP HYPERPARAMETER TUNING
 final_model_e = XGBRegressor(**{**grid_search1_e.best_params_, **grid_search2_e.best_params_}, n_jobs=40)
 # UNCOMMENT TO USE OUR FOUND OPTIMAL HYPERPARAMETERS
-# e_best_params = {'colsample_bytree': np.float64(1.0), 'max_depth': np.int64(9), 'min_child_weight': np.int64(1), 'subsample': np.float64(1.0), 'learning_rate': 0.05, 'n_estimators': 2500}
+# e_best_params = {'colsample_bytree': np.float64(1.0), 'max_depth': np.int64(9), 'min_child_weight': np.int64(2), 'subsample': np.float64(1.0), 'learning_rate': 0.05, 'n_estimators': 2500}
 # final_model_e = XGBRegressor(**e_best_params, n_jobs=40)
 
 final_model_e.fit(trainX_e, trainY_e)
@@ -96,6 +107,7 @@ pth_e = Path("data/models/best_model_e_final.xgb")
 final_model_e.save_model(str(pth_e))
 # %%
 # COMMENT OUT THIS SECTION TO SKIP HYPERPARAMETER TUNING
+print("Fitting INC")
 start = time.time()
 grid_search1_inc = GridSearchCV(estimator=XGBRegressor(random_state=42, learning_rate=0.3, n_estimators=500, n_jobs=40),
                            param_grid=param1_grid, cv=5, scoring="neg_mean_squared_error", verbose=1)
@@ -108,12 +120,10 @@ grid_search2_inc = GridSearchCV(estimator=XGBRegressor(random_state=42, **grid_s
 grid_search2_inc.fit(trainX_inc, trainY_inc)
 
 end = time.time()
-print(f"Best score: {grid_search2_inc.best_score_:.3}")
+print(f"Best score: {grid_search2_inc.best_score_:.3}") # this is MSE not RMSE
 print(f"Best parameters: {grid_search1_inc.best_params_ | grid_search2_inc.best_params_}")
 print("Optimization Time: %.2f seconds" % (end - start))
 # %%
-# final_model_inc = grid_search2_inc.best_estimator_
-
 # COMMENT OUT TO SKIP HYPERPARAMETER TUNING
 final_model_inc = XGBRegressor(**{**grid_search1_inc.best_params_, **grid_search2_inc.best_params_}, n_jobs=40)
 # UNCOMMENT TO USE OUR FOUND OPTIMAL HYPERPARAMETERS
@@ -127,6 +137,7 @@ final_model_inc.fit(trainX_inc, trainY_inc)
 pth_inc = Path("data/models/best_model_inc_final.xgb")
 final_model_inc.save_model(str(pth_inc))
 # %%
+# THIS IS REPORTED AS MSE, NOT RMSE
 e_best_idx = grid_search2_e.best_index_
 e_score_mean = grid_search2_e.cv_results_["mean_test_score"][e_best_idx]
 e_score_std = grid_search2_e.cv_results_["std_test_score"][e_best_idx]

2.2_predict_test.py
@@ -4,21 +4,17 @@ import numpy as np
 import pandas as pd
 
 import xgboost as xgb
-
-from sklearn.model_selection import train_test_split
 # %%
 merged_df = pd.read_csv("data/merged_elements.csv", index_col=0, dtype={"Des'n": str})
 
-features_e = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'prope_h']
-features_inc = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'propsini_h']
-data_e = merged_df[features_e]
-data_inc = merged_df[features_inc]
-dele = merged_df['prope']-merged_df['e']
-delsini = merged_df['propsini']-np.sin(merged_df['Incl.']*np.pi/180)
-delg = merged_df['g0'] - merged_df['g']
-s = merged_df['s']
+features_e = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'prope_linear']
+features_inc = ['sinicosO', 'sinisinO', 'ecospo', 'esinpo', 'a', 'propsini_linear']
 
-trainX_e, testX_e, trainX_inc, testX_inc, trainY_e, testY_e, trainY_inc, testY_inc = train_test_split(data_e, data_inc, dele, delsini, train_size=0.8, random_state=42)
+merged_df_test = merged_df[merged_df["test_set"] == 1]
+testX_e = merged_df_test[features_e]
+testY_e = merged_df_test['prope']-merged_df_test['e'] # dele
+testX_inc = merged_df_test[features_inc]
+testY_inc = merged_df_test['propsini']-np.sin(np.deg2rad(merged_df_test["Incl."])) # delsini
 # %%
 final_model_e = xgb.XGBRegressor()
 final_model_e.load_model("data/models/best_model_e_final.xgb")
@@ -37,7 +33,7 @@ pred_e = final_model_e.predict(testX_e)
 pred_inc = final_model_inc.predict(testX_inc)
 eval_t = time.process_time() - start_t
 print(f"Model Evaluation Time: {eval_t:.2f} sec for {len(testX_e)} asteroids. {eval_t/len(testX_e):.4} sec / asteroid")
-# Model Evaluation Time: 56.06 sec for 249811 asteroids. 0.0002244 sec / asteroid
+# Model Evaluation Time: 47.05 sec for 249811 asteroids. 0.0001883 sec / asteroid
 
 test_indices = testX_e.index.tolist()
 
@@ -53,18 +49,17 @@ df_xgb["a"] = test_data["a"]
 df_xgb["Incl."] = test_data["Incl."]
 df_xgb["Node"] = test_data["Node"]
 df_xgb["Peri."] = test_data["Peri."]
-df_xgb["M"] = test_data["M"]
 
 # linear
-df_xgb["prope_h"] = test_data["prope_h"]
-df_xgb["propsini_h"] = test_data["propsini_h"]
+df_xgb["prope_linear"] = test_data["prope_linear"]
+df_xgb["propsini_linear"] = test_data["propsini_linear"]
 
 # predicted
 pred_e = df_xgb["pred_dele"] + df_xgb["e"]
 pred_e[pred_e < 0] = 0 # clamp values less than 0
 df_xgb["pred_e"] = pred_e
 
-pred_sini = df_xgb["pred_delsini"] + np.sin((df_xgb["Incl."] * np.pi/180))
+pred_sini = df_xgb["pred_delsini"] + np.sin((np.deg2rad(df_xgb["Incl."])))
 df_xgb["pred_sini"] = pred_sini
 
 # acutal